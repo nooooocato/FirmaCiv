802b24187a4e7cd01cb9089ae0635c3a10564415
Traister101 <66151907+Traister101@users.noreply.github.com>
2024-04-08 19:42:38 -0700

    Cleanup tags

--- src/main/java/com/alekiponi/firmaciv/util/FirmacivTags.java
+++ src/main/java/com/alekiponi/firmaciv/util/FirmacivTags.java
@@ -8,7 +8,6 @@ import net.minecraft.world.level.block.Block;
 
 public class FirmacivTags {
 
-
     public static class Blocks {
 
         public static final TagKey<Block> CAN_MAKE_CANOE = create("can_make_canoe");
@@ -16,15 +15,9 @@ public class FirmacivTags {
         public static final TagKey<Block> CANOE_COMPONENT_BLOCKS = create("canoe_component_blocks");
         public static final TagKey<Block> CAN_MAKE_CANOE_UNRESTRICTED = create("can_make_canoe_unrestricted");
 
-        public static final TagKey<Block> WOODEN_BOAT_FRAMES = create("wooden_watercraft_frames");
-
-        public Blocks() {
-        }
-
         private static TagKey<Block> create(String id) {
             return TagKey.create(Registries.BLOCK, new ResourceLocation("firmaciv", id));
         }
-
     }
 
     public static class Items {
@@ -33,15 +26,7 @@ public class FirmacivTags {
         public static final TagKey<Item> AXES = getFromTFC("axes");
         public static final TagKey<Item> LUMBER = getFromTFC("lumber");
 
-        public static final TagKey<Item> ANVILS = getFromTFC("anvils");
-
-        public static final TagKey<Item> PLANKS = create("planks");
         public static final TagKey<Item> CHESTS = create("chests");
-        public static final TagKey<Item> WORKBENCHES = create("workbenches");
-        public static final TagKey<Item> SHULKER_BOX = create("shulker_box");
-        public static final TagKey<Item> CAN_PLACE_IN_COMPARTMENTS = create("can_place_in_compartments");
-
-        public static final TagKey<Item> PLANKS_THAT_MAKE_SHIPS = create("planks_that_make_ships");
 
         public static TagKey<Item> getFromTFC(String id) {
             return TagKey.create(Registries.ITEM, new ResourceLocation("tfc", id));
